ba2971892f49e75ae8db1ba0959d72b3af2e6bcc
FbcGa <148731381+FbcGa@users.noreply.github.com>
2024-05-28 03:27:00 +0000

    button-fixed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	const [inputValue, setInputValue] = useState('');
+	const [tasks, setTasks] = useState([]);
+
+	const handleChange =(e) => {
+		setInputValue(e.target.value);
+	}
+
+	const addTask =(e) => {
+		if(e.key === "Enter"){
+			setTasks([...tasks, inputValue]);
+			setInputValue(" ");
+		}
+	}
+
+	const deleteTask = (index) => {
+		setTasks(tasks.filter(( _,newIndex)=>newIndex !== index))
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<section>
+			<h1>TO DO LIST </h1>
+			<input
+			value={inputValue} 
+			onChange={ (e) => handleChange(e)} 
+			onKeyDown={(e) => addTask(e)}
+			type="text" className="form-control" placeholder="Tareas pendientes" aria-label="Username" aria-describedby="basic-addon1"/>
+			<ul>
+			{
+				tasks.map((task,index)=>( <li>{task} <button  onClick={() => deleteTask(index)} className="btn btn-danger"><i class="fas fa-trash"></i></button> </li>))
+			}
+			
+			</ul>
+			
+		</section>
+		
 	);
 };
 
