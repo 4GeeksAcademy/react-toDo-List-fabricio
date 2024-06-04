3d4e1e842a1f95fc3b90d90abd1e4f4d73b09cd2
FbcGa <fabricio.alipazaga.pe@gmail.com>
2024-06-04 11:46:29 -0500

    ra

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 
 //create your first component
@@ -7,38 +7,38 @@ const Home = () => {
 	const [inputValue, setInputValue] = useState('');
 	const [tasks, setTasks] = useState([]);
 
-	const handleChange =(e) => {
+	const handleChange = (e) => {
 		setInputValue(e.target.value);
 	}
 
-	const addTask =(e) => {
-		if(e.key === "Enter"){
+	const addTask = (e) => {
+		if (e.key === "Enter") {
 			setTasks([...tasks, inputValue]);
 			setInputValue(" ");
 		}
 	}
 
 	const deleteTask = (index) => {
-		setTasks(tasks.filter(( _,newIndex)=>newIndex !== index))
+		setTasks(tasks.filter((_, newIndex) => newIndex !== index))
 	}
 
 	return (
 		<section>
 			<h1>TO DO LIST </h1>
 			<input
-			value={inputValue} 
-			onChange={ (e) => handleChange(e)} 
-			onKeyDown={(e) => addTask(e)}
-			type="text" className="form-control" placeholder="Tareas pendientes" aria-label="Username" aria-describedby="basic-addon1"/>
+				value={inputValue}
+				onChange={(e) => handleChange(e)}
+				onKeyDown={(e) => addTask(e)}
+				type="text" className="form-control" placeholder="Tareas pendientes" aria-label="Username" aria-describedby="basic-addon1" />
 			<ul>
-			{
-				tasks.map((task,index)=>( <li>{task} <button  onClick={() => deleteTask(index)} className="btn btn-danger"><i class="fas fa-trash"></i></button> </li>))
-			}
-			
+				{
+					tasks.map((task, index) => (<li>{task} <button onClick={() => deleteTask(index)} className="btn btn-danger"><i class="fas fa-trash"></i></button> </li>))
+				}
+
 			</ul>
-			
+
 		</section>
-		
+
 	);
 };
 
